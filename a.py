2bb7f7649641d6cfead56acc60c3a4e58bd0b47e
quynhanhnp <qanh21032007@gmail.com>
2024-05-26 11:47:24 +0700

    first commit

---
+++ a.py
@@ -0,0 +1,64 @@
+#phan 1
+#bai 1
+# def ham(a):
+#    if a % 2 == 0:
+#      return true
+#    else:
+#      return False   
+# a = int(input("nhap so: "))
+# if ham(a):
+#   print("chan")
+# else:
+#   print("le")
+
+#bai 2
+# def cal_area(r):
+#     S = 3.14 * r
+#     return S
+# r = float(input("nhập bán kính hình tròn: "))
+# print(cal_area(r))
+
+#bai 3
+# def  reverse_str(a):
+#     for i in a:
+#      i.sort(reverse = True)
+#     return i
+# a = str( input("nhập: "))
+# print(reverse_str(a))
+
+#bai 4
+
+
+
+
+#phan 2
+#bai 1
+
+
+#bai 2
+# a = [5, 1, 8, 92, -1, 30]
+# def thislist(a):
+#     a.sort()
+#     return a
+# print(thislist(a))  
+#bai 3
+# def  print_fibo(n):
+#     for i in n:
+      
+#         return
+# n = int(input("nhap so:"))
+# print(print_fibo(n))
+
+
+#phan 3
+#bai 1
+# def number(a,b):
+#     n = a-b
+#     return n
+# a = int(input("nhap a: "))
+# b = int(input("nhap b: "))
+# if  number(a,b) > 17:
+#     print(number(a,b)*2)
+# elif  number(a,b) < 0:
+#     print(number(a,b)*-1)   
+#bai 2
